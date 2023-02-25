010a9fc291f0fbc947e57a52ea265e5e77639cb4
coin6442@gmail.com <coin6442@gmail.com>
2023-02-25 09:18:06 +0900

    fix : 라우터 수정

--- controller/Cplan.js
+++ controller/Cplan.js
@@ -3,7 +3,7 @@ const { Plan, User } = require('../models')
 module.exports = {
     getMyPlans: async (req, res) => {
         try {
-            const userId = req.userId;
+            const userId = req.body.userId;
             const owner = await User.findOne({
                 where: {userId: userId}
             }) 
@@ -26,12 +26,92 @@ module.exports = {
     },
     createMyPlans: async (req, res) => {
         
+        try {
+            const userId = req.body.userId;
+            const owner = await User.findOne({
+                where: {userId: userId}
+            }) 
+            const { 
+                day,
+                start,
+                end,
+                color
+            } = req.body;
+
+            if(!day || !start || !end || !color) {
+                res.status(400).send(`Cannot get body`);
+            }
+
+            const newPlan = await Plan.create({
+                day,
+                start,
+                end,
+                color,
+                owner
+            })
+
+            return res.status(200).json({
+                ok: true,
+                id,
+                name: owner.name,
+                day,
+                start,
+                end,
+                start,
+            })
+
+        } catch(e) {
+            console.log(e)
+            res.status(500).json(e)
+        }
     },
     getOthersPlans: async (req, res) => {
         
     },
     createOthersPlans: async (req, res) => {
         
+        async (req, res) => {
+            try {
+                const userId = req.body.userId;
+                const owner = await User.findOne({
+                    where: {userId: userId}
+                }) 
+                const { 
+                    name,
+                    day,
+                    start,
+                    end,
+                    color
+                } = req.body;
+    
+                if(!day || !start || !end || !color) {
+                    res.status(400).send(`Cannot get body`);
+                }
+    
+                const newPlan = await Plan.create({
+                    name,
+                    day,
+                    start,
+                    end,
+                    color,
+                    owner
+                })
+    
+                return res.status(200).json({
+                    ok: true,
+                    id,
+                    name: owner.name,
+                    day,
+                    start,
+                    end,
+                    start,
+                })
+    
+            } catch(e) {
+                console.log(e)
+                res.status(500).json(e)
+            }
+        }
     },
     getMyLinks: async (req, res) => {
         

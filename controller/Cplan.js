c9f3a183407c5194c3a5c9ebe2fe8505799d5923
coin6442@gmail.com <coin6442@gmail.com>
2023-02-25 11:19:41 +0900

    fix : gitignore 수정

--- controller/Cplan.js
+++ controller/Cplan.js
@@ -3,7 +3,8 @@ const { Plan, User } = require('../models')
 module.exports = {
     getMyPlans: async (req, res) => {
         try {
-            const userId = req.body.userId;
+            const userId = req.header('userId');
+
             if (userId == undefined) {
                 return res.json({
                     ok: false,
@@ -15,11 +16,11 @@ module.exports = {
             }) 
             const plans = await Plan.findAll({
                 where: {
-                    owner: owner
+                    owner: owner.userId
                 },
-                attributes : ['name']
             }).then((res) => res.map((value)=> value.dataValues));
             
+            console.log("plan!!!!!!!!!!", plans)
             return res.status(200).json({
                 ok: true,
                 owner:owner.name,
@@ -32,7 +33,13 @@ module.exports = {
     },
     createMyPlans: async (req, res) => {
         try {
-            const userId = req.userId;
+            const userId = req.header('userId');
+            if (userId == undefined) {
+                return res.json({
+                    ok: false,
+                    message: "userId가 없습니다."
+                })
+            }
             const owner = await User.findOne({
                 where: {userId: userId}
             }) 
@@ -49,54 +56,16 @@ module.exports = {
 
             const newPlan = await Plan.create({
                 day,
-                start,
-                end,
-                color,
-                owner
-            })
-
-            return res.status(200).json({
-                ok: true,
-                id,
                 name: owner.name,
-                day,
-                start,
-                end,
-                start,
-            })
-
-        } catch(e) {
-            console.log(e)
-            res.status(500).json(e)
-        }
-        
-        try {
-            const userId = req.body.userId;
-            const owner = await User.findOne({
-                where: {userId: userId}
-            }) 
-            const { 
-                day,
-                start,
-                end,
-                color
-            } = req.body;
-
-            if(!day || !start || !end || !color) {
-                res.status(400).send(`Cannot get body`);
-            }
-
-            const newPlan = await Plan.create({
-                day,
                 start,
                 end,
                 color,
-                owner
+                owner: owner.userId
             })
 
             return res.status(200).json({
                 ok: true,
-                id,
+                id: newPlan.id,
                 name: owner.name,
                 day,
                 start,
@@ -112,35 +81,20 @@ module.exports = {
     getOthersPlans: async (req, res) => {
         try {
             const userId = req.params.userId;
+            console.log("userId : ", userId)
+            if (!userId) {
+                return res.json({
+                    ok: false,
+                    message: "userId가 없습니다."
+                })
+            }
             const owner = await User.findOne({
                 where: {userId: userId}
             }) 
             const plans = await Plan.findAll({
                 where: {
-                    owner: owner
-                },
-                attributes : ['name']
-            }).then((res) => res.map((value)=> value.dataValues));
-            
-            return res.status(200).json({
-                ok: true,
-                owner:owner.name,
-                plans
-            })
-        } catch(e) {
-            console.log(e)
-            res.status(500).json(e)
-        }
-        try {
-            const userId = req.params.userId;
-            const owner = await User.findOne({
-                where: {userId: userId}
-            }) 
-            const plans = await Plan.findAll({
-                where: {
-                    owner: owner
-                },
-                attributes : ['name']
+                    owner: owner.userId
+                }
             }).then((res) => res.map((value)=> value.dataValues));
             
             return res.status(200).json({
@@ -154,12 +108,19 @@ module.exports = {
         }
     },
     createOthersPlans: async (req, res) => {
-        async (req, res) => {
-            try {
-                const userId = req.body.userId;
+        try {
+                const userId = req.params.userId;
+                console.log()
+                if (userId == undefined) {
+                    return res.json({
+                        ok: false,
+                        message: "userId가 없습니다."
+                    })
+                }
                 const owner = await User.findOne({
                     where: {userId: userId}
                 }) 
+                console.log("owner: ", owner)
                 const { 
                     name,
                     day,
@@ -167,40 +128,43 @@ module.exports = {
                     end,
                     color
                 } = req.body;
-    
+
                 if(!day || !start || !end || !color) {
                     res.status(400).send(`Cannot get body`);
                 }
-    
+
                 const newPlan = await Plan.create({
                     name,
                     day,
                     start,
                     end,
                     color,
-                    owner
+                    owner: owner.userId
                 })
-    
+
                 return res.status(200).json({
                     ok: true,
-                    id,
-                    name: owner.name,
+                    id: newPlan.id,
+                    name: newPlan.name,
                     day,
                     start,
                     end,
-                    start,
                 })
-    
-            } catch(e) {
-                console.log(e)
-                res.status(500).json(e)
-            }
+
+        } catch(e) {
+            console.log(e)
+            res.status(500).json(e)
         }
     },
     getMyLinks: async (req, res) => {
         try {
-            const userId = req.body.userId;
-
+            const userId = req.header('userId');
+            if (userId == undefined) {
+                return res.json({
+                    ok: false,
+                    message: "userId가 없습니다."
+                })
+            }
             const link = `https://www.spring-time/plans/${userId}`
 
             res.status(200).json({
